314e4d37c0b2e947159518ae468df18f9794a9eb
Boom <ssangbomb@gmail.com>
2023-04-13 14:40:29 +0000

    finally snake moves

--- input.js
+++ input.js
@@ -1,5 +1,8 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
     const stdin = process.stdin;
+    connection = conn;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
     stdin.resume();
@@ -7,11 +10,26 @@ const setupInput = function () {
     return stdin;
   };
 
+
+
 const handleUserInput = function (key) {
   // your code here
   if (key === '\u0003') {
       process.exit();
   }
+  if (key === "w") {
+    connection.write('Move: up');
+  }
+  if (key === "a") {
+    connection.write('Move: left');
+  }
+  if (key === "s") {
+    connection.write('Move: down');
+  }
+  if (key === "d") {  
+    connection.write('Move: right');
+  }
 };
+
   
-module.exports = setupInput, handleUserInput
+module.exports = { setupInput }
