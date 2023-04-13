e6f9c36c52420017a32d36faa0980462bd8d8c67
Boom <ssangbomb@gmail.com>
2023-04-13 13:32:06 +0000

    I put my name on snake

--- client.js
+++ client.js
@@ -4,12 +4,17 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: 'localhost',
-    port: 50541
+    port: 50541,
+    
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server")
+    conn.write('Name: JMC')
+  })
+  
   return conn;
 };
 
